14b68ea943dee2b6535964d1de400cc93b53c4d5
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-03-06 14:43:57 +0530

    Added Shooting fireballs in both directions correctly

---
+++ Fireball.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+const SPEED = 100
+
+var velocity = Vector2()
+
+#Direction of the Fireball
+var direction = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+#set the direction of the Fireball facing
+func set_fireball_direction(dir):
+	direction = dir
+	if direction == -1:
+		$Fireball_Ani.flip_h=true
+	else:
+		$Fireball_Ani.flip_h=false
+		
+
+func _physics_process(delta):
+	velocity.x = SPEED * delta * direction
+	$Fireball_Ani.play("Shoot")
+	translate(velocity)
+
+#Fire this even when Fireball moves out of screen
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+#Fire this even when Fireball collision body collides with another
+func _on_Fireball_body_entered(_body):
+	queue_free()
--- Player.gd
+++ Player.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const FIREBALL = preload("res://Fireball.tscn")
+
 const SPEED = 60
 const GRAVITY = 10
 const JUMP_FORCE = -250
@@ -9,16 +11,20 @@ const FLOOR = Vector2(0,-1)
 var velocity = Vector2()
 var is_on_ground =false
 
-func _physics_process(delta):	
+func _physics_process(_delta):	
 #	X axis movement (MOVE)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
 		$Player_Anim.flip_h=false		
 		$Player_Anim.play("Run")
+		if sign($ShootPos.position.x) == -1:
+			$ShootPos.position.x *= -1
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x=-SPEED
 		$Player_Anim.flip_h=true
-		$Player_Anim.play("Run")		
+		$Player_Anim.play("Run")
+		if sign($ShootPos.position.x) == 1:
+			$ShootPos.position.x *= -1		
 	else:
 		velocity.x=0
 		if is_on_ground:
@@ -28,7 +34,16 @@ func _physics_process(delta):
 #	Y axis movement (JUMP)
 	if Input.is_action_pressed("ui_up") and is_on_ground:
 		velocity.y = JUMP_FORCE
-	
+		
+#	Shoot Fireballs
+	if Input.is_action_just_pressed("ui_select"):
+		var fireball = FIREBALL.instance()
+		if sign($ShootPos.position.x) == 1:
+			fireball.set_fireball_direction(1)
+		else:
+			fireball.set_fireball_direction(-1)
+		get_parent().add_child(fireball)
+		fireball.position = $ShootPos.global_position
 #	Grvity increasing per frame
 	velocity.y += GRAVITY
 	
