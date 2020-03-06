d0df8deea60e6afd8ba6a9a664da798107d22737
Charith Rathnayake <charith.n.rathnayake@gmail.com>
2020-03-06 11:58:30 +0530

    Added Basic RUN,JUMP,FALL,IDLE player animations

--- Player.gd
+++ Player.gd
@@ -13,10 +13,16 @@ func _physics_process(delta):
 #	X axis movement (MOVE)
 	if Input.is_action_pressed("ui_right"):
 		velocity.x = SPEED
+		$Player_Anim.flip_h=false		
+		$Player_Anim.play("Run")
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x=-SPEED
+		$Player_Anim.flip_h=true
+		$Player_Anim.play("Run")		
 	else:
 		velocity.x=0
+		if is_on_ground:
+			$Player_Anim.play("Idle")
 		
 		
 #	Y axis movement (JUMP)
@@ -30,6 +36,10 @@ func _physics_process(delta):
 		is_on_ground=true
 	else:
 		is_on_ground=false
+		if velocity.y < 0:
+			$Player_Anim.play("Jump")
+		else:
+			$Player_Anim.play("Fall")
 	
 #	By setting velocity to return value of move and slide...
 #	it retains it's position after a jump or falling down
